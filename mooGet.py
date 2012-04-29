81e15ec0e055f4a939fea519f1a8e25b5bfece02
Mookie Kong <mookie@ymail.com>
2012-04-29 14:39:43 -0700

    creating a mysql datetime string

--- mooGet.py
+++ mooGet.py
@@ -3,14 +3,14 @@
 import urllib
 import json
 
-month_dict = {"Jan":1,"Feb":2,"Mar":3,"Apr":4, "May":5, "Jun":6, "Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12}
+month_dict = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06','Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
 
 def to_dict(name):
    return month_dict[name]
 
 fields = ['id','created_at','text']
 user = 'ultramookie'
-count = '20'
+count = '200'
 
 url = 'https://api.twitter.com/1/statuses/user_timeline.json?screen_name=' + user + '&count=' + count + '&trim_user=true'
 twitreturn = urllib.urlopen(url)
@@ -27,5 +27,6 @@ for entity in twitjson:
 			mon = to_dict(datesplit[1])
 			day = datesplit[2]
 			time = datesplit[3]
+			mdatetime = year + '-' + mon + '-' + day + ' ' + time
 
-	print 'id: ' + str(tweetid) + '\ntext: ' + tweettext + '\ndate: ' + str(mon) + ' ' + day + ' ' + year + ' ' + time + '\n'
+	print 'id: ' + str(tweetid) + '\ntext: ' + tweettext + '\ndate: ' + mdatetime + '\n'
